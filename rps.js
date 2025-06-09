d6a739d6809b43b841731184d718b61770c99ea5
harleysbeatrice <54000119+harleysbeatrice@users.noreply.github.com>
2025-06-09 12:14:48 +0300

    Functionality, game log, score updates

--- rps.js
+++ rps.js
@@ -5,41 +5,58 @@ function getComputerChoice() {
         (intChoice === 1) ? "Paper" : "Scissors";
 }
 
-function capitalize (str) {
-    str = str.toLowerCase()
-    return str.slice(0,1).toUpperCase() + str.slice(1,);
-}
-function getHumanChoice() {
-    let seed = prompt("Choose your weapon: Rock, Paper or Scissors?")
-    return capitalize(seed);
-}
+let playerName = document.querySelector(".player.profile h2");
+const nameInput = prompt("What is your name?");
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    for (let i = 1; i <= 5; i++) {
-        const human = getHumanChoice();
-        const comp = getComputerChoice();
-        if(human === comp) {
-            humanScore += 1;
-            computerScore += 1;
-            console.log("Draw! Everyone gets a point.")
-        }else if((human ==="Rock" && comp=== "Scissors") || (human === "Scissors" && comp === "Paper") ||
-            (human === "Paper" && comp ==="Rock")) {
-            humanScore += 1;
-            console.log(`You Win! ${human} beats ${comp}. \nYour Score: ${humanScore} \nComputer's Score: ${computerScore}`)
-        }else {
-            computerScore += 1;
-            console.log(`You Lose! ${human} beats ${comp}. \nYour Score: ${humanScore} \nComputer's Score: ${computerScore}`)
+playerName.textContent = (nameInput === "") ? "Stranger" : nameInput;
+
+let computerScoreDisplay = document.querySelector(".score.computer h2");
+let playerScoreDisplay = document.querySelector(".score.player h2");
+const buttons = document.querySelectorAll("button");
+const logList = document.querySelector(".log-list");
+
+let humanScore = 0;
+let compScore = 0;
+
+function playGame(playerMove) {
+    const human = playerMove;
+    const comp = getComputerChoice();
+    if(human === comp) {
+        let log = document.createElement("li");
+        log.textContent = "It's a Draw!";
+        if (logList.childElementCount >= 5) {
+            logList.lastElementChild.remove();
         }
+        logList.insertBefore(log,logList.children[0]);
+    } else if(
+        (human === "Rock" && comp === "Scissors") ||
+        (human === "Scissors" && comp === "Paper") ||
+        (human === "Paper" && comp === "Rock")
+    ) {
+        humanScore += 1;
+        let log = document.createElement("li");
+        log.textContent = `You Win! ${human} beats ${comp}. \nYour Score: ${humanScore} \nComputer's Score: ${compScore}`
+        if (logList.childElementCount >= 5) {
+            logList.lastElementChild.remove();
+        }
+        logList.insertBefore(log,logList.children[0]);
+    } else {
+        compScore += 1;
+        let log = document.createElement("li");
+        log.textContent = `You Lose! ${human} beats ${comp}. \nYour Score: ${humanScore} \nComputer's Score: ${compScore}`
+        if (logList.childElementCount >= 5) {
+            logList.lastElementChild.remove();
+        }
+        logList.insertBefore(log,logList.children[0]);
     }
-    if(humanScore > computerScore) {
-        console.log(`FINAL SCORES\nYour Score: ${humanScore}\nComputer's Score: ${computerScore} \nYou Win!!!`)
-    } else if(humanScore === computerScore) {
-        console.log(`FINAL SCORES\nYour Score: ${humanScore}\nComputer's Score: ${computerScore} \nIt's a Draw :(`)
-    }else {
-        console.log(`FINAL SCORES\nYour Score: ${humanScore}\nComputer's Score: ${computerScore} \nYou Lose :(`)
-    }
+    computerScoreDisplay.textContent = compScore.toString();
+    playerScoreDisplay.textContent = humanScore.toString();
+    return;
 }
 
-playGame()
+buttons.forEach((button) => {
+    const playerMove = button.textContent;
+    button.addEventListener("click", () => {
+        playGame(playerMove);
+    })
+})
